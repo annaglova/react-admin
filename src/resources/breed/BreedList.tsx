c99b8b37c14b281c2193a51c8f164d7fd3b16686
annaglova <anna.maliyenko@gmail.com>
2025-05-31 15:28:35 +0300

    Refactor BreedList component: remove unused fields and clean up imports for improved readability

--- src/resources/breed/BreedList.tsx
+++ src/resources/breed/BreedList.tsx
@@ -1,7 +1,6 @@
 import {
   Datagrid,
   List,
-  NumberField,
   ReferenceField,
   TextField,
   TextInput,
@@ -22,31 +21,9 @@ export const BreedList = () => (
       <ReferenceField source="pet_type_id" reference="pet_type">
         <TextField source="name" />
       </ReferenceField>
-      <ReferenceField source="language_id" reference="sys_language">
-        <TextField source="name" />
-      </ReferenceField>
-      <TextField source="authentic_name" />
-
-      <NumberField source="pet_profile_count" />
-      <ReferenceField source="category_id" reference="breed_category">
-        <TextField source="name" />
-      </ReferenceField>
-      <NumberField source="kennel_count" />
-
       <TextField source="admin_name" />
       <TextField source="url" />
       <ReferenceField source="account_id" reference="account">
-        <TextField source="name" />
-      </ReferenceField>
-      <NumberField source="patron_count" />
-      <TextField source="avatar_url" />
-      <NumberField source="rating" />
-      <NumberField source="achievement_progress" />
-      <ReferenceField source="cover_id" reference="cover">
-        <TextField source="name" />
-      </ReferenceField>
-      <NumberField source="payment_rating" />
-      <ReferenceField source="public_data_id" reference="public_data">
         <TextField source="name" />
       </ReferenceField>
     </Datagrid>
